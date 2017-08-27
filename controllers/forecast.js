47c82a021c5699add06da33dda7dc142f575034f
Robert Zehnder <robert@kisdigital.com>
2017-08-27 15:07:54 -0400

    - Pay schedule is now working

--- controllers/forecast.js
+++ controllers/forecast.js
@@ -10,47 +10,56 @@ exports.getIndex = (req, res) => {
 
             if (err) { return next(err); }
             let ref = new Moment();
-            let startOfWeek = new Moment().startOf('week').subtract(7 - 5, 'days');
-
-            if(ref.diff(startOfWeek, 'days') >= 7) {
-                startOfWeek.add(7, 'days');
-            }
-
-            let endOfWeek = new Moment(startOfWeek).add(6, 'days');
-            
             let weekly = [];
 
-            let weeklyTemplate = {
-                start: startOfWeek.format('dddd, MMMM Do YYYY'),
-                end: endOfWeek.format('dddd, MMMM Do YYYY'),
-                amountDue: 0,
-                count: 0,
-                payees: []
-            };
+            for(let otr = 0; otr < 4; otr++){
+                if(otr) ref.add(1, 'weeks');
 
-            for(var i = 0; i < payees.length; i++){
-                let diff = new Moment().diff(payees[i].ref, "months");
-                let eventDate = new Moment(payees[i].ref).add(diff, 'months');
+                let startOfWeek = new Moment(ref).startOf('week').subtract(7 - 5, 'days');
 
-                if(eventDate.isBefore(startOfWeek)){
-                    eventDate.add(1, 'month');
+                if (ref.diff(startOfWeek, 'days') >= 7) {
+                    startOfWeek.add(7, 'days');
                 }
-                // One day before start
-                let t = new Moment(startOfWeek).subtract(1, 'day');
 
-                if (eventDate.isBetween(t, endOfWeek)) {
-                    weeklyTemplate.payees.push(payees[i]);
-                    weeklyTemplate.amountDue += payees[i].amount;
-                    weeklyTemplate.count++;
+                let endOfWeek = new Moment(startOfWeek).add(6, 'days');
+
+                let weeklyTemplate = {
+                    start: startOfWeek.format('dddd, MMMM Do YYYY'),
+                    end: endOfWeek.format('dddd, MMMM Do YYYY'),
+                    amountDue: 0,
+                    count: 0,
+                    payees: []
+                };
+
+                for (let i = 0; i < payees.length; i++) {
+                    let diff = new Moment().diff(payees[i].ref, "months");
+                    let eventDate = new Moment(payees[i].ref).add(diff, 'months');
+
+                    if (eventDate.isBefore(startOfWeek)) {
+                        eventDate.add(1, 'month');
+                    }
+                    // One day before start
+                    let ts = new Moment(startOfWeek).subtract(1, 'day');
+                    let te = new Moment(endOfWeek).add(1, 'day');
+
+                    if (eventDate.isBetween(ts, te)) {
+                        weeklyTemplate.payees.push(payees[i]);
+                        weeklyTemplate.amountDue += payees[i].amount;
+                        weeklyTemplate.count++;
+                    }
                 }
+
+                weekly.push(weeklyTemplate);
+
+
             }
 
-            weekly.push(weeklyTemplate);
+            console.dir(weekly);
 
             res.render('forecast/weekly', {
                 title: 'Forecast',
-                startOfWeek: startOfWeek.format('dddd MMMM Do YYYY'),
-                endOfWeek: endOfWeek.format('dddd MMMM Do YYYY'),
+                // startOfWeek: startOfWeek.format('dddd MMMM Do YYYY'),
+                // endOfWeek: endOfWeek.format('dddd MMMM Do YYYY'),
                 weeks: weekly
             });
 
