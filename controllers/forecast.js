8d2b11e0590a85da67f35f9df1fc29f8f6f2d53a
Robert Zehnder <robert@kisdigital.com>
2017-09-08 09:11:11 -0400

    Mark items paid in the weekly template after a payment has been made

--- controllers/forecast.js
+++ controllers/forecast.js
@@ -1,74 +1,91 @@
 
 const Payee = require('../models/Payee');
+const Payment = require('../models/Payment');
 const formatCurrency = require('format-currency');
-const Moment = require('moment');
+const Moment = require('moment-timezone');
 
 exports.getIndex = (req, res) => {
     Payee
         .find({ owner: req.user._id }, null, { sort: { ref: 1 } }, (err, payees) => {
 
             if (err) { return next(err); }
-            let ref = new Moment();
-            let weekly = [];
-            let monthlyTotal = payees.reduce((acc, payee) => acc + payee.amount, 0);
+            Payment
+                .find({ owner: req.user._id }, null, { sort: { ref: 1 } }, (err, payments) => {
 
-            for(let otr = 0; otr < 4; otr++){
-                if(otr) ref.add(1, 'weeks');
+                    if (err) { return next(err); }
+                    let ref = new Moment();
+                    let weekly = [];
+                    let monthlyTotal = payees.reduce((acc, payee) => acc + payee.amount, 0);
 
-                let startOfWeek = new Moment(ref).startOf('week').subtract(7 - req.user.offset, 'days');
+                    for(let otr = 0; otr < 4; otr++){
+                        if(otr) ref.add(1, 'weeks');
 
-                if (ref.diff(startOfWeek, 'days') >= 7) {
-                    startOfWeek.add(7, 'days');
-                }
+                        let startOfWeek = new Moment(ref).startOf('week').subtract(7 - req.user.offset, 'days');
 
-                let endOfWeek = new Moment(startOfWeek).add(6, 'days');
+                        if (ref.diff(startOfWeek, 'days') >= 7) {
+                            startOfWeek.add(7, 'days');
+                        }
 
-                let weeklyTemplate = {
-                    start: startOfWeek.format('dddd, MMMM Do YYYY'),
-                    end: endOfWeek.format('dddd, MMMM Do YYYY'),
-                    amountDue: 0,
-                    count: 0,
-                    payees: []
-                };
+                        let endOfWeek = new Moment(startOfWeek).add(6, 'days');
 
-                for (let i = 0; i < payees.length; i++) {
-
-                    let diff = new Moment().diff(payees[i].ref, 'months');
-                    let eventDate = new Moment(payees[i].ref).add(diff, 'months');
-                    if (eventDate.isBefore(startOfWeek)) {
-                        eventDate.add(1, 'month');
-                    }
-
-                    // ts and te is one day before and one day after so dates fall in between
-                    let ts = new Moment(startOfWeek).subtract(1, 'day');
-                    let te = new Moment(endOfWeek).add(1, 'day');
-                    if (eventDate.isBetween(ts, te)) {
-                        let payeeData = {
-                            id: payees[i].id,
-                            date: eventDate,
-                            name: payees[i].name,
-                            url: payees[i].url,
-                            description: payees[i].description,
-                            amount: payees[i].amount
+                        let weeklyTemplate = {
+                            start: startOfWeek.format('dddd, MMMM Do YYYY'),
+                            end: endOfWeek.format('dddd, MMMM Do YYYY'),
+                            amountDue: 0,
+                            count: 0,
+                            payees: []
                         };
 
-                        weeklyTemplate.payees.push(payeeData);
-                        weeklyTemplate.amountDue += payees[i].amount;
-                        weeklyTemplate.count++;
+                        for (let i = 0; i < payees.length; i++) {
+
+                            let diff = new Moment().diff(payees[i].ref, 'months');
+                            let eventDate = new Moment(payees[i].ref).add(diff, 'months');
+                            if (eventDate.isBefore(startOfWeek)) {
+                                eventDate.add(1, 'month');
+                            }
+
+
+
+                            
+
+                            // ts and te is one day before and one day after so dates fall in between
+                            let ts = new Moment(startOfWeek).subtract(1, 'day');
+                            let te = new Moment(endOfWeek).add(1, 'day');
+                            if (eventDate.isBetween(ts, te)) {
+
+                                let isPaid = payments.filter((payment) => {
+                                    return payment.payee == payees[i].id && payment.ref == eventDate.format('YYYY-MM-DD');
+                                });
+                                
+                                let payeeData = {
+                                    id: payees[i].id,
+                                    date: eventDate,
+                                    name: payees[i].name,
+                                    url: payees[i].url,
+                                    description: payees[i].description,
+                                    amount: payees[i].amount,
+                                    isPaid: isPaid.length ? true : false
+                                };
+
+                                weeklyTemplate.payees.push(payeeData);
+                                weeklyTemplate.amountDue += payees[i].amount;
+                                weeklyTemplate.count++;
+                            }
+                        }
+                        weeklyTemplate['percentMonth'] = (weeklyTemplate.amountDue / monthlyTotal) * 100;
+
+                        weekly.push(weeklyTemplate);
+
+
                     }
-                }
-                weeklyTemplate['percentMonth'] = (weeklyTemplate.amountDue / monthlyTotal) * 100;
 
-                weekly.push(weeklyTemplate);
-
-
-            }
-
-            res.render('forecast/weekly', {
-                title: 'Forecast',
-                monthlyTotal: monthlyTotal,
-                weeks: weekly
-            });
+                    res.render('forecast/weekly', {
+                        title: 'Forecast',
+                        monthlyTotal: monthlyTotal,
+                        weeks: weekly
+                    });
+                
+                });
 
         });
 
