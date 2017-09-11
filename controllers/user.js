e1e782924f95b1957f3daed136f57d12554f544d
Robert Zehnder <robert@kisdigital.com>
2017-09-11 17:54:30 -0400

    Adding start of week to user profile

--- controllers/user.js
+++ controllers/user.js
@@ -141,6 +141,7 @@ exports.postUpdateProfile = (req, res, next) => {
     user.profile.gender = req.body.gender || '';
     user.profile.location = req.body.location || '';
     user.profile.website = req.body.website || '';
+    user.offset = req.body.offset || 0;
     user.save((err) => {
       if (err) {
         if (err.code === 11000) {
